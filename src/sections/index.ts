b47fa8524bca6df9759c92e9bac5d4629cebe29b
abd-saad <4bd.saad1999@gmail.com>
2026-09-25 21:11:06 +0700

    feat: export solutions and blog sections

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -4,5 +4,16 @@ import { HeroSection } from './HeroSection';
 import { ExperienceSection } from './ExperienceSection';
 import { ProjectSection } from './ProjectSection';
 import { CertificationSection } from './CertificationSection';
+import { SolutionsSection } from './SolutionsSection';
+import { BlogSection } from './BlogSection';
 
-export { SkillsSection, ContactSection, HeroSection, ExperienceSection, ProjectSection, CertificationSection };
+export {
+  SkillsSection,
+  ContactSection,
+  HeroSection,
+  ExperienceSection,
+  ProjectSection,
+  CertificationSection,
+  SolutionsSection,
+  BlogSection,
+};
