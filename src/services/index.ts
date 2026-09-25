8cf9baaf2d25486d24dbe05ad5fed78fe7b35676
abd-saad <4bd.saad1999@gmail.com>
2026-09-25 21:03:26 +0700

    feat: export solutions and blog services

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,5 +6,21 @@ import { getProjects } from './experience';
 import { getSkillCategoriesWithSkills } from './skills';
 import { fetchSocialLinks } from './socialLinks';
 import { getCertifications } from './certifications';
+import { getFeaturedSolutions, getSolutions, getSolutionBySlug } from './solutions';
+import { getLatestBlogPosts, getBlogPosts, getBlogPostBySlug } from './blog';
 
-export { getHomepage, getExperiences, getProjects, getSkillCategoriesWithSkills, fetchSocialLinks, getCertifications, getProfileImage };
+export {
+  getHomepage,
+  getExperiences,
+  getProjects,
+  getSkillCategoriesWithSkills,
+  fetchSocialLinks,
+  getCertifications,
+  getProfileImage,
+  getFeaturedSolutions,
+  getSolutions,
+  getSolutionBySlug,
+  getLatestBlogPosts,
+  getBlogPosts,
+  getBlogPostBySlug,
+};
