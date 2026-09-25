d14d010e9b1b31360b24ac242f3c161ea7a8f58d
abd-saad <4bd.saad1999@gmail.com>
2026-09-25 21:11:15 +0700

    feat: render solutions and blog homepage sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
-import { HeroSection, SkillsSection, ContactSection, ExperienceSection, ProjectSection, CertificationSection } from '@/sections';
-import { getHomepage } from '@/services/homepage'
-import { THomepage } from '@/types/homepage'
+import {
+  HeroSection,
+  SkillsSection,
+  ContactSection,
+  ExperienceSection,
+  ProjectSection,
+  CertificationSection,
+  SolutionsSection,
+  BlogSection,
+} from '@/sections';
+import { getHomepage } from '@/services/homepage';
+import { THomepage } from '@/types/homepage';
 
 export default async function Home() {
   const homepage = await getHomepage();
-  return (
-    <>
-      {homepage.map((homepageItem) => renderHomepage(homepageItem))}
-    </>
-  )
+
+  return <>{homepage.map((homepageItem) => renderHomepage(homepageItem))}</>;
 }
 
 const renderHomepage = (homepage: THomepage) => {
-  console.log('Rendering homepage section:', homepage.section_type);
   switch (homepage.section_type) {
     case 'hero':
       return <HeroSection key={homepage.id} content={homepage} />;
@@ -22,8 +27,12 @@ const renderHomepage = (homepage: THomepage) => {
       return <ExperienceSection key={homepage.id} content={homepage} />;
     case 'projects':
       return <ProjectSection key={homepage.id} content={homepage} />;
+    case 'solutions':
+      return <SolutionsSection key={homepage.id} content={homepage} />;
     case 'certifications':
       return <CertificationSection key={homepage.id} content={homepage} />;
+    case 'blog':
+      return <BlogSection key={homepage.id} content={homepage} />;
     case 'contact':
       return <ContactSection key={homepage.id} content={homepage} />;
     default:
